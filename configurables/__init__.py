c8885d5126363e2d2032d043b2fe85454616bc70
William Fong <willfong@mit.edu>
2022-09-20 14:32:51 -0500

    updating top level imports

--- configurables/__init__.py
+++ configurables/__init__.py
@@ -5,10 +5,19 @@ from configurables.configurable import (
     define_option,
     define_param,
 )
+from configurables.parse import CFG, CLI, ENV
 
 __author__ = """William Christopher Fong"""
 __email__ = "willfong@mit.edu"
 __version__ = "0.1.0"
 
 
-__all__ = ["configure", "define_param", "define_option", "configurable"]
+__all__ = [
+    "configure",
+    "define_param",
+    "define_option",
+    "configurable",
+    "ENV",
+    "CFG",
+    "CLI",
+]
