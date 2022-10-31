5f84e08ec82299f38713dc91e267eecc2d077b69
William Fong <willfong@mit.edu>
2022-10-31 07:46:24 -0600

    bumping version

--- configurables/__init__.py
+++ configurables/__init__.py
@@ -4,7 +4,7 @@ from configurables.parse import CFG, CLI, ENV
 
 __author__ = """William Christopher Fong"""
 __email__ = "willfong@mit.edu"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 
 __all__ = [
