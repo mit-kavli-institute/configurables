205a88494597e650b9f7c7f682a204a0cdb1d47c
William Fong <willfong@mit.edu>
2022-11-21 09:34:19 -0700

    making setup.py recognize version number in package

--- setup.py
+++ setup.py
@@ -2,43 +2,68 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+import codecs
+import os.path
 
-with open('README.rst') as readme_file:
+from setuptools import find_packages, setup
+
+
+# See https://packaging.python.org/guides/single-sourcing-package-version/
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="William Christopher Fong",
-    author_email='willfong@mit.edu',
-    python_requires='>=3.6',
+    author_email="willfong@mit.edu",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    description="A quick package to create factories using configuration files of various syntaxes.",
+    description=(
+        "A quick package to create factories using configuration "
+        "files of various syntaxes."
+    ),
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='configurables',
-    name='configurables',
-    packages=find_packages(include=['configurables', 'configurables.*']),
-    test_suite='tests',
+    keywords="configurables",
+    name="configurables",
+    packages=find_packages(include=["configurables", "configurables.*"]),
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/WilliamCFong/configurables',
-    version='0.1.0',
+    url="https://github.com/WilliamCFong/configurables",
+    version=get_version("configurables/__init__.py"),
     zip_safe=False,
 )
